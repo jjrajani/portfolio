d9ccbdefc9c1adaaa6f6c1afcab408cde5b6ff92
humdrum <jjrajani@gmail.com>
2017-09-14 22:15:27 -0400

    add twin bridges lake project

--- src/app/App.js
+++ src/app/App.js
@@ -4,39 +4,73 @@ import {
   Route,
   Redirect,
   Switch,
-
   Link
 } from 'react-router-dom';
 import './app.scss';
-import { VARS } from "./VARS";
-import { Contact, Detail, Home, Projects, Resume, Nav, Footer } from "./components";
+import { VARS } from './VARS';
+import {
+  Contact,
+  Detail,
+  Home,
+  Projects,
+  Resume,
+  Nav,
+  Footer
+} from './components';
 
 class App extends Component {
-
   render() {
     return (
       <Router>
         <div id="app">
           <div className="app-header">
             <div className="left">
-              <Link to={VARS.routePrefix + "/home"} className="name">Jenna Rajani</Link>
+              <Link to={VARS.routePrefix + '/home'} className="name">
+                Jenna Rajani
+              </Link>
               <div className="bottom">
                 <p>Web Developer</p>
-                <a href="http://www.github.com/jjrajani" target="blank"><i className="fa fa-github" aria-hidden="true"/></a>
+                <a
+                  href="http://www.github.com/jjrajani"
+                  target="blank"
+                  alt="Jenna Rajani web developer github link"
+                >
+                  <i className="fa fa-github" aria-hidden="true" />
+                </a>
               </div>
             </div>
             <Nav />
           </div>
           <Switch>
-            <Redirect exact from={VARS.redirect} to={VARS.routePrefix + "/home"}/>
-            <Redirect from={"/portfolio"} to={VARS.routePrefix + "/home"}/>
-            <Route exact path={VARS.routePrefix + "/home"} component={Home}/>
-            <Route exact path={VARS.routePrefix + "/resume"} component={Resume}/>
-            <Route exact path={VARS.routePrefix + "/projects"} component={Projects}/>
-            <Route exact path={VARS.routePrefix + "/project/:projectName"} component={Detail}/>
-            <Route exact path={VARS.routePrefix + "/contact"} component={Contact}/>
+            <Redirect
+              exact
+              from={VARS.redirect}
+              to={VARS.routePrefix + '/home'}
+            />
+            <Redirect from={'/portfolio'} to={VARS.routePrefix + '/home'} />
+            <Route exact path={VARS.routePrefix + '/home'} component={Home} />
+            <Route
+              exact
+              path={VARS.routePrefix + '/resume'}
+              component={Resume}
+            />
+            <Route
+              exact
+              path={VARS.routePrefix + '/projects'}
+              component={Projects}
+            />
+            <Route
+              exact
+              path={VARS.routePrefix + '/project/:projectName'}
+              component={Detail}
+            />
+            <Route
+              exact
+              path={VARS.routePrefix + '/contact'}
+              component={Contact}
+            />
           </Switch>
-          <Footer/>
+          <Footer />
         </div>
       </Router>
     );
