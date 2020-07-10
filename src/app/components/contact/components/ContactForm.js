d53b60365d68014ae84e6905515b9abc1b9a144e
Jenna Rajani <jjrajani@gmail.com>
2020-07-10 01:15:23 -0400

    fix emailer

--- src/app/components/contact/components/ContactForm.js
+++ src/app/components/contact/components/ContactForm.js
@@ -1,50 +1,100 @@
-import React from 'react';
-import Fields from './formFields';
+import React from "react";
+import Fields from "./formFields";
+import axios from "axios";
 
-const ContactForm = () =>
-    <div>
+class ContactForm extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isSending: false,
+      didSend: false,
+      didError: false
+    };
+  }
+  onSubmit = e => {
+    e.preventDefault();
+    this.setState({ didSend: false, didError: false, isSending: true });
+    let data = {};
+    let i = 0;
+    while (i < this.form.elements.length - 1 /* skip submit button */) {
+      const { name, value } = this.form.elements[i];
+      data[name] = value;
+      i++;
+    }
+
+    axios
+      .post(`${process.env.REACT_APP_EMAIL_API}send-email`, data)
+      .then(res => {
+        if (res.status === 200 || res.status === 204) {
+          this.setState({ didSend: true, isSending: false, didError: false });
+        } else {
+          this.setState({ didError: true, isSending: false, didSend: false });
+        }
+      });
+  };
+  render() {
+    const disabled = this.state.isSending;
+    return (
+      <div>
         <p className="message">Send me a message</p>
-        <form
-            id="form"
-            action="https://script.google.com/macros/s/AKfycbyhthAKEEKBnlqLKVFsyfv6wmej4Q_0PKisv_S3xOv4YDtYCVw/exec"
-            encType="multipart/form-data"
-            method="POST"
-        >
-            <ul>
-                {Fields.map((f, i) => {
-                    return (
-                        <li key={`${f.id}-${i}`}>
-                            {f.label &&
-                                <label htmlFor={f.label.for}>
-                                    {f.label.text}
-                                </label>}
-
-                            {f.input.type !== 'textarea' &&
-                                <input
-                                    type={f.input.type}
-                                    name={f.input.name}
-                                    id={f.input.id}
-                                    placeholder={f.input.placeholder}
-                                    required={f.input.required}
-                                />}
-                            {f.input.type === 'textarea' &&
-                                <textarea
-                                    type={f.input.type}
-                                    name={f.input.name}
-                                    id={f.input.id}
-                                    placeholder={f.input.placeholder}
-                                    required={f.input.required}
-                                />}
-                        </li>
-                    );
-                })}
-            </ul>
+        {this.state.didError && (
+          <p className="error">
+            Oops! Something when wrong when seding your email. <br />
+            If the problem persists give me a call at{" "}
+            <a href="tel:6786404262">(678) 640-4262</a>.
+          </p>
+        )}
+        <form onSubmit={this.onSubmit} ref={c => (this.form = c)}>
+          <ul>
+            {Fields.map((f, i) => {
+              return (
+                <li
+                  key={`${f.id}-${i}`}
+                  className={`input-wrapper${
+                    f.input.type === "submit" ? " submit" : ""
+                  }`}
+                >
+                  {f.label && (
+                    <label htmlFor={f.label.for}>{f.label.text}</label>
+                  )}
+                  {this.state.didSend && f.input.type === "submit" && (
+                    <li id="thankyou_message">
+                      <p>
+                        Thanks for reaching out! I'll get back to you shortly.
+                      </p>
+                    </li>
+                  )}
+                  {f.input.type !== "textarea" && (
+                    <input
+                      type={f.input.type}
+                      name={f.input.name}
+                      id={f.input.id}
+                      placeholder={f.input.placeholder}
+                      required={f.input.required}
+                      value={f.input.value}
+                      disabled={disabled}
+                    />
+                  )}
+                  {f.input.type === "textarea" && (
+                    <textarea
+                      type={f.input.type}
+                      name={f.input.name}
+                      id={f.input.id}
+                      placeholder={f.input.placeholder}
+                      required={f.input.required}
+                      value={f.input.value}
+                      disabled={disabled}
+                    />
+                  )}
+                </li>
+              );
+            })}
+          </ul>
         </form>
-        <div className="hidden" id="thankyou_message">
-            <h2>
-                <em>Thanks</em> for contacting us! We will get back to you soon!
-            </h2>
-        </div>
-    </div>;
+      </div>
+    );
+  }
+}
 
 export default ContactForm;
--- src/app/components/home/Home.js
+++ src/app/components/home/Home.js
@@ -6,32 +6,34 @@ import * as c from "./components";
 
 const logo = require("../../assets/lost-in-the-trees.png");
 
-const Home = () => (
-  <div className="main-content">
-    <div id="home">
-      <div className="sub-content top">
-        <PageHeader
-          direction={"next"}
-          page="Home"
-          link={{ href: "/resume", text: "Resumé" }}
-        />
-        <c.PageTitle />
-        <div className="right">
-          <div className="home-image">
-            <img src={logo} alt="Lost in the trees" />
+const Home = () => {
+  return (
+    <div className="main-content">
+      <div id="home">
+        <div className="sub-content top">
+          <PageHeader
+            direction={"next"}
+            page="Home"
+            link={{ href: "/resume", text: "Resumé" }}
+          />
+          <c.PageTitle />
+          <div className="right">
+            <div className="home-image">
+              <img src={logo} alt="Lost in the trees" />
+            </div>
           </div>
         </div>
-      </div>
-      <div className="sub-content right">
-        <PageHeader
-          direction={"next"}
-          page="Projects"
-          link={{ href: "/projects", text: "See more" }}
-        />
-        <c.ProjectsList />
+        <div className="sub-content right">
+          <PageHeader
+            direction={"next"}
+            page="Projects"
+            link={{ href: "/projects", text: "See more" }}
+          />
+          <c.ProjectsList />
+        </div>
       </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default scrollToTop(googleAnalytics("/home", Home));
